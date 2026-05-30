c4161b224ede08d2a82db2cc707c4002e3bee589
Kainat Zahra <kainatzahra1012@gmail.com>
2026-05-30 23:28:01 +0500

    connect todo routes to server

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.json({message:'API is running...'})
-})
+//routes
+const todoRoutes = require('./routes/todos');
+app.use('/api/todos', todoRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
